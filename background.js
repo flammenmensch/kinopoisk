05ebefef5e6eec137762db370ebe1e656ef77468
Flammenmensch <maleventum@gmail.com>
2021-10-07 12:07:55 +0300

    Add .editorconfig, reformat code

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'OPEN') {
-        const url = message.url.replace(/(www\.)?kinopoisk/, 'ggkinopoisk');
-        chrome.tabs.create({
-            url,
-        });
-        sendResponse({
-            ok: true
-        });
-        return true;
-    }
+  if (message.type === 'OPEN') {
+    const url = message.url.replace(/(www\.)?kinopoisk/, 'ggkinopoisk');
+    chrome.tabs.create({
+      url,
+    });
+    sendResponse({
+      ok: true
+    });
+    return true;
+  }
 });
--- contentScript.js
+++ contentScript.js
@@ -1,19 +1,19 @@
 const createButton = () => {
-    const button = document.createElement('button');
-    button.innerText = 'Смотреть бесплатно';
-    button.classList.add('ss-button');
+  const button = document.createElement('button');
+  button.innerText = 'Смотреть бесплатно';
+  button.classList.add('ss-button');
 
-    return button;
+  return button;
 };
 
 const buttonClickHandler = () => {
-    button.innerText = 'Подождите...';
-    chrome.runtime.sendMessage({
-        type: 'OPEN',
-        url: window.location.href
-    }, () => {
-        button.innerText = 'Смотреть бесплатно';
-    });
+  button.innerText = 'Подождите...';
+  chrome.runtime.sendMessage({
+    type: 'OPEN',
+    url: window.location.href
+  }, () => {
+    button.innerText = 'Смотреть бесплатно';
+  });
 };
 
 const button = createButton();
