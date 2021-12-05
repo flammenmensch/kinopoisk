ed370a05a691f981d8a4642b0958317793c018bc
Flammenmensch <maleventum@gmail.com>
2021-12-05 19:13:24 +0300

    Update domain

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'OPEN') {
-    const url = message.url.replace(/(www\.)?kinopoisk/, 'ggkinopoisk');
+    const url = message.url.replace(/(www\.)?kinopoisk/, 'kinopoiskk');
     chrome.tabs.create({
       url,
     });
